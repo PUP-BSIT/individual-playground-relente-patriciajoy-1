04a97bbd1ee15465fa56ae2e6f7c06393039d6a2
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-09-14 19:06:04 +0800

    feature: add PetCategory enum

--- ts-pet-profiles/src/script.ts
+++ ts-pet-profiles/src/script.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+export enum PetCategory {
+  DOG,
+  CAT,
+  BIRD,
+  FISH,
+  OTHER
+}
+
+export type PetProfile = {
+  id: number;
+  name: string;
+  owner: string;
+  age: number;
+  category: PetCategory;
+  favoriteFood?: string;
+  print: () => void;
+}
